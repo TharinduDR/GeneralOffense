3f4a45d544fb97e6601315eb25ccefebf824df30
TharinduDR <rhtdranasinghe@gmail.com>
2021-11-04 16:39:21 +0000

    001: Initial Commit

--- data/HATEVAL/hateval.py
+++ data/HATEVAL/hateval.py
@@ -7,7 +7,7 @@ from sklearn.model_selection import train_test_split
 
 from deepoffense.classification import ClassificationModel
 
-from data.HASOC.hasoc_config import TEMP_DIRECTORY, MODEL_TYPE, MODEL_NAME, args, SEED
+from data.HATEVAL.hateval_config import TEMP_DIRECTORY, MODEL_TYPE, MODEL_NAME, args, SEED
 from util.TestInstance import TestInstance
 from util.evaluation import macro_f1, weighted_f1
 from util.label_converter import encode, decode
