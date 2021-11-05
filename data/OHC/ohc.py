6da9229ba2943bab9e549f3f65d860092f6e6e18
TharinduDR <rhtdranasinghe@gmail.com>
2021-11-05 00:19:04 +0000

    001: Initial Commit

--- data/OHC/ohc.py
+++ data/OHC/ohc.py
@@ -80,7 +80,7 @@ if args["evaluate_during_training"]:
         model.train_model(train_df, eval_df=eval_df, macro_f1=macro_f1, weighted_f1=weighted_f1,
                           accuracy=sklearn.metrics.accuracy_score)
         model = ClassificationModel(MODEL_TYPE, args["best_model_dir"], args=args,
-                                    use_cuda=torch.cuda.is_available(), cuda_device=1)
+                                    use_cuda=torch.cuda.is_available(), cuda_device=2)
 
         for test_instance in test_instances:
             print()
