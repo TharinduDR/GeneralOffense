7b5f66304272752ccee96be0cea7ed5e5a7afa21
TharinduDR <rhtdranasinghe@gmail.com>
2021-11-10 09:24:36 +0000

    001: Initial Commit

--- data/HASOC/leave_hasoc.py
+++ data/HASOC/leave_hasoc.py
@@ -78,11 +78,11 @@ if args["evaluate_during_training"]:
         train_df, eval_df = train_test_split(train, test_size=0.2, random_state=SEED * i)
         model = ClassificationModel(MODEL_TYPE, MODEL_NAME, args=args,
                                     use_cuda=torch.cuda.is_available(),
-                                    cuda_device=2)
+                                    cuda_device=0)
         model.train_model(train_df, eval_df=eval_df, macro_f1=macro_f1, weighted_f1=weighted_f1,
                           accuracy=sklearn.metrics.accuracy_score)
         model = ClassificationModel(MODEL_TYPE, args["best_model_dir"], args=args,
-                                    use_cuda=torch.cuda.is_available(), cuda_device=2)
+                                    use_cuda=torch.cuda.is_available(), cuda_device=0)
 
         for test_instance in test_instances:
             predictions, raw_outputs = model.predict(test_instance.get_sentences())
