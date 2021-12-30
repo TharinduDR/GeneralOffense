298ed004e79bce4f58b1c95039fd8b1b6514d4ca
TharinduDR <rhtdranasinghe@gmail.com>
2021-12-30 14:51:38 +0000

    001: Initial Commit

--- util/TestInstance.py
+++ util/TestInstance.py
@@ -7,8 +7,8 @@ class TestInstance:
         self.name = name
         self.df = df
         self.args = args
-        self.df = self.df.rename(columns={'Text': 'text', 'Class': 'labels'})
-        self.df['labels'] = encode(self.df["labels"])
+        # self.df = self.df.rename(columns={'Text': 'text', 'Class': 'labels'})
+        # self.df['labels'] = encode(self.df["labels"])
         self.test_preds = np.zeros((len(self.df), self.args["n_fold"]))
 
     def get_sentences(self):
