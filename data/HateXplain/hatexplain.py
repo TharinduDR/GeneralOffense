bbf2e90e25edc931c7d7d58d9a39c6c7d0cb215e
TharinduDR <rhtdranasinghe@gmail.com>
2022-01-09 19:43:25 +0000

    001: Initial Commit

--- data/HateXplain/hatexplain.py
+++ data/HateXplain/hatexplain.py
@@ -78,7 +78,7 @@ if args["evaluate_during_training"]:
 
         model = ClassificationModel(MODEL_TYPE, MODEL_NAME, args=args,
                                     use_cuda=torch.cuda.is_available(),
-                                    cuda_device=0)
+                                    cuda_device=1)
 
         train_df, eval_df = train_test_split(train, test_size=0.2, random_state=SEED * i)
         model.train_model(train_df, eval_df=eval_df, macro_f1=macro_f1, weighted_f1=weighted_f1,
